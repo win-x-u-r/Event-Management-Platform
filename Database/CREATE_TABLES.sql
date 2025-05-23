6ad9633cc33a38fa158de00e7bda9ebfdebfb941
win-x-u-r <134261446+win-x-u-r@users.noreply.github.com>
2025-05-23 10:33:05 +0400

    reformatting

--- Database/CREATE_TABLES.sql
+++ Database/CREATE_TABLES.sql
@@ -1,3 +1,12 @@
+CREATE TABLE IF NOT EXISTS BUDGET_TABLE (
+    Budget_ID INTEGER PRIMARY KEY,
+    Item_Name VARCHAR(100),
+    Item_Quantity INTEGER,
+    Item_Cost DECIMAL(10, 2),
+    Total_Cost DECIMAL(10, 2),
+    Budget_Status VARCHAR(50)
+);
+
 CREATE TABLE IF NOT EXISTS EVENT_TABLE (
     Event_ID INTEGER PRIMARY KEY,
     Event_Name VARCHAR(100),
@@ -10,8 +19,9 @@ CREATE TABLE IF NOT EXISTS EVENT_TABLE (
     Available_Seats INTEGER,
     Event_Status VARCHAR(50),
     Event_Description TEXT,
-    Event_Host VARCHAR(100)
-    -- Budget_ID INTEGER FOREIGN KEY REFERENCES BUDGET_TABLE(Budget_ID),
+    Budget_ID INTEGER,
+    Event_Host VARCHAR(100),
+    FOREIGN KEY (Budget_ID) REFERENCES BUDGET_TABLE(Budget_ID)
 );
 
 CREATE TABLE IF NOT EXISTS USER_TABLE (
@@ -19,23 +29,15 @@ CREATE TABLE IF NOT EXISTS USER_TABLE (
     User_First_Name VARCHAR(50),
     User_Last_Name VARCHAR(50),
     User_Department VARCHAR(100),
-    User_Role VARCHAR(20),
+    User_Role VARCHAR(20)
 );
 
 CREATE TABLE IF NOT EXISTS MEDIA_TABLE (
     Media_ID INTEGER PRIMARY KEY,
     Media_URL TEXT,
-    MEDIA_TYPE VARCHAR(50),
+    Media_Type VARCHAR(50),
     Media_Name VARCHAR(100),
-    User_ID INTEGER FOREIGN KEY REFERENCES USER_TABLE(User_ID),
+    User_ID INTEGER REFERENCES USER_TABLE(User_ID),
     Media_Size INTEGER
 );
 
-CREATE TABLE IF NOT EXISTS BUDGET_TABLE (
-    Budget_ID INTEGER PRIMARY KEY,
-    Item_Name VARCHAR(100),
-    Item_Quantity INTEGER,
-    Item_Cost DECIMAL(10, 2),
-    Total_Cost DECIMAL(10, 2),
-    Budget_Status VARCHAR(50),
-);
