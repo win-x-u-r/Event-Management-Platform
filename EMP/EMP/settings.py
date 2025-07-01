397597adff5a48c034172df730d6cfb71aaaab2b
win-x-u-r <134261446+win-x-u-r@users.noreply.github.com>
2025-07-01 09:18:21 +0400

    ip fixes

--- EMP/EMP/settings.py
+++ EMP/EMP/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t!w%c1@c4gg78sr9&rmpbe+*h@!-zcvpo%9f0156gpx-i=)&ul
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.16.1.39', 'localhost', '192.168.1.104']
+ALLOWED_HOSTS = ['172.16.1.34', 'localhost', '192.168.1.104']
 
 
 # Application definition
@@ -160,5 +160,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'internbot2AURAK@gmail.com'
-EMAIL_HOST_PASSWORD = 'ycrt ljml sgmu hpax'  # 🔒 Not your Gmail password!
+EMAIL_HOST_PASSWORD = 'ycrt ljml sgmu hpax'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- FrontEnd/event-form-beautifier/src/config.ts
+++ FrontEnd/event-form-beautifier/src/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "http://172.16.1.39:8000";
+export const API_BASE_URL = "http://172.16.1.34:8000";
